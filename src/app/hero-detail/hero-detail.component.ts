ee146bad7bea6db25c5cb6cf422bb520e1546286
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-15 00:25:03 -0400

    FormController replaced with FormGroup

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl ,FormGroup} from '@angular/forms';
+import { FormBuilder ,FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'hero-detail',
@@ -7,25 +7,17 @@ import { FormControl ,FormGroup} from '@angular/forms';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  /** FormControl:
-   *   is a directive that allows to create and manage a FormControl instance directly.
-   * FormControl constructor accepts three, optional arguments:
-   *    the initial data value,
-   *    an array of validators, and
-   *    an array of async validators.
-   */ 
-  /**
-   * create a FormControl called name.
-   * It will be bound in the template to an HTML input box for the hero name.
-   * This simple control doesn't have data or validators.
-   * In real apps, most form controls have both.
-   */
-  
-  heroForm = new FormGroup({
-    name : new FormControl()
-  });
+  heroForm: FormGroup; // <--- heroForm is of type FormGroup
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { // <--- inject FormBuilder
+    this.createForm();
+  }
+
+  createForm() {
+    this.heroForm = this.fb.group({
+      name: '', // <--- the FormControl called "name"
+    });
+  }
 
   ngOnInit() {
   }
