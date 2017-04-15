42c2a84a5d581c6bc450e8ac065ec2d899725103
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-14 22:35:30 -0400

    created a FormControl called name

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  /** FormControl:
+   *   is a directive that allows to create and manage a FormControl instance directly.
+   * FormControl constructor accepts three, optional arguments:
+   *    the initial data value,
+   *    an array of validators, and
+   *    an array of async validators.
+   */ 
+  /**
+   * create a FormControl called name.
+   * It will be bound in the template to an HTML input box for the hero name.
+   * This simple control doesn't have data or validators.
+   * In real apps, most form controls have both.
+   */
+  name = new FormControl();
 
   constructor() { }
 
