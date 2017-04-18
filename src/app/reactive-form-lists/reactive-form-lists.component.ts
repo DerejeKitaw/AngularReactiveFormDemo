2576c27308bfeda00ed11b555bc0d99c0f610ca9
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-17 23:24:42 -0400

    finally is fixed by importing RX - this will import all rxjs TODO: update it

--- src/app/reactive-form-lists/reactive-form-lists.component.ts
+++ src/app/reactive-form-lists/reactive-form-lists.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs/Observable'
+import {Observable} from 'rxjs/RX'
 import {Hero} from '../data-model'
 import {HeroService} from '../hero.service'
 @Component({
