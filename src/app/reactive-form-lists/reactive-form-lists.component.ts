1fff48236ab6d80d9e1a9b87e6c1ce007c0daa7d
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-04-17 22:04:32 -0400

    reactive form list populated

--- src/app/reactive-form-lists/reactive-form-lists.component.ts
+++ src/app/reactive-form-lists/reactive-form-lists.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form-lists.component.css']
 })
 export class ReactiveFormListsComponent implements OnInit {
-
+//Lists of reactive form
+  demos: string[] = [
+    'Just a FormControl',
+    'FormControl in a FormGroup',
+    'Simple FormBuilder group',
+    'Group with multiple controls',
+    'Nested FormBuilder group',
+    'PatchValue',
+    'SetValue',
+    'FormArray',
+  ];
+  
   constructor() { }
 
   ngOnInit() {
